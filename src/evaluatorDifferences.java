1bac0457a8fe0dabd86f6974e6510e4dcfa4c0ae
Andrey V <vinyaykin@gmail.com>
2018-10-25 22:40:21 +0300

    0.1.1 рефакторинг

--- src/evaluatorDifferences.java
+++ src/evaluatorDifferences.java
@@ -6,13 +6,14 @@ public class evaluatorDifferences {
     public static String rv = ";";
     public static String rs = "\n";
     public static String fileName = "evaluatorDifferences.csv";
+    public static int filesCount;
 
     public static void main(String[] args) {
         try {
             System.out.println("Введите количество сравниваемых файлов");
-            int n = setIntInput();
+            filesCount = setIntInput();
 
-            BufferedReader files[] = setArrayFiles(n);
+            BufferedReader files[] = setArrayFiles(filesCount);
             String csv = diffFiles(files);
 
             PrintWriter pw = new PrintWriter(new File(fileName));
@@ -50,6 +51,9 @@ public class evaluatorDifferences {
             System.out.println("Введите название файла для сравнения №" + Integer.toString(i+1));
             try {
                 name = in.next();
+                if (name.equals("exit")) {
+                    System.exit(0);
+                }
                 BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream(name)));
                 files[i] = file;
                 i++;
@@ -64,28 +68,21 @@ public class evaluatorDifferences {
         String[] lineCsv;
         String str;
         StringBuilder sb = new StringBuilder();
-        lineCsv = new String[files.length];
+        lineCsv = setFirstLineCSV(filesCount);
+        appendLine(sb, lineCsv);
+        int nStr = 0;
 
         try {
-            sb.append("Номер строки" + rv);
-            for (int i=0; i<files.length; i++) {
-                lineCsv[i] = "Файл " + Integer.toString(i+1);
-            }
-            appendLine(sb, lineCsv);
-
-            int nStr = 0;
-            while (++nStr > 0) {
-                for (int i=0; i<files.length; i++) {
-                    lineCsv[i] =files[i].readLine();
+            do {
+                nStr++;
+                lineCsv[0] = Integer.toString(nStr);
+                for (int i=0; i < filesCount; i++) {
+                    lineCsv[i+1] =files[i].readLine();
                 }
-                if (checkNullLines(lineCsv)) {
-                    break;
-                }
-                if (diffLines(lineCsv)) {
-                    sb.append(Integer.toString(nStr) + rv);
+                if (diffLines(Arrays.copyOfRange(lineCsv, 1, filesCount+1))) {
                     appendLine(sb, lineCsv);
                 }
-            }
+            } while (checkNullLines(Arrays.copyOfRange(lineCsv, 1, filesCount+1)));
         } catch (IOException e) {
             System.out.println("Ошибка");
             System.exit(0);
@@ -93,27 +90,32 @@ public class evaluatorDifferences {
         return sb.toString();
     }
 
+    public static String[] setFirstLineCSV(int n) {
+        String line[] = new String[n + 1];
+        line[0] = "Номер строки";
+        for (int i=1; i <= n; i++) {
+            line[i] = "Файл " + Integer.toString(i);
+        }
+        return line;
+    }
+
     public static boolean diffLines(String[] lineCsv) {
-        int flag = 0;
-        String str1 = lineCsv[0] != null ? lineCsv[0] : "";
+        String str = lineCsv[0] != null ? lineCsv[0] : "";
         for (int i=1; i< lineCsv.length; i++) {
-            if (!str1.equals(lineCsv[i])) {
-                flag = 1;
-                break;
+            if (!str.equals(lineCsv[i] != null ? lineCsv[i] : "")) {
+                return true;
             }
         }
-        return flag == 1;
+        return false;
     }
 
     public static boolean checkNullLines(String[] lineCsv) {
-        int flag = 0;
         for (int i=0; i< lineCsv.length; i++) {
             if (lineCsv[i] !=null) {
-                flag = 1;
-                break;
+                return true;
             }
         }
-        return flag == 0;
+        return false;
     }
 
     public static void appendLine(StringBuilder sb, String[] arr) {
